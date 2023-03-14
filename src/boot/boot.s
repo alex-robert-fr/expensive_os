84d2128be18ffc519d00953c7982cce74e5af2a9
Alex Robert <alrobert@1C2.42angouleme.fr>
2023-03-14 23:23:36 +0100

    Bientot le passage en 32-bits

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -3,25 +3,27 @@ org 0x7c00
 jmp _start
 include 'print.inc'
 include 'disk.inc'
+include '32bit_gdt.inc'
+include '32bit_switch.inc'
 _start:
-    mov bp, 0x8000
+    mov bp, 0x9000
     mov sp, bp
 
-    mov bx, 0x9000
-    mov dh, 2
-    call disk_load
+    mov bx, MSG_REAL_MODE
+    call println
 
-    mov dx, [0x9000]
-    call print_hex
-
-    mov dx, [0x9000 + 512]
-    call print_hex
+    call switch_to_pm
 
     jmp $
+
+use32
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+
+    MSG_REAL_MODE: db 'Demarrage en 16-bits mode reel', 0
+    MSG_PROT_MODE: db 'Demarrage en 32-bits mode protege', 0
 
     ; Remplie 510 octets 0, depuis l'adresse du début du programme($$) jusqu'à l'adresse actuel du programme($)
     times 510-($-$$) db 0
     dw 0xaa55
-
-    times 256 dw 0xdada
-    times 256 dw 0xface
