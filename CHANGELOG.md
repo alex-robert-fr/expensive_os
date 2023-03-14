5b5e584ebbb3165c8d401f99ff48386369168d19
Alex Robert <contact.alexrobert04@gmail.com>
2023-03-14 03:31:55 +0100

    add disk load

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,19 @@
 # Changelog
+## [0.0.4a] = (14.04.2023)
+### Ajouts
+- Fichier `disk.inc`
+- Ajout d'une fonction `load_disk`
+### Changements
+- `boot.s` charge maintenant le second secteur du disque dur
+- la fonction `print_hex` affiche un retour chariot a la fin du nombre hexadecimal
+
 ## [0.0.3a] - (13.03.2023)
 ### Ajouts
 - Fichier `print.inc`
 - Ajout d'une fonction `print`
 - Ajout d'une fonction `println`
 - Ajout d'une fonction `print_hex`
-### Changement
+### Changements
 - `boot.s` démarre maintenant a partir de l'adresse `0x7c00`
 ### Fix
 - Nom du fichier `.gitignore` modifier (.gitgore -> .gitignore)
--- src/boot/boot.s
+++ src/boot/boot.s
@@ -2,23 +2,26 @@ org 0x7c00
 
 jmp _start
 include 'print.inc'
+include 'disk.inc'
 _start:
-    mov bx, titre
-    call println
+    mov bp, 0x8000
+    mov sp, bp
 
-    mov bx, title
-    call println
+    mov bx, 0x9000
+    mov dh, 2
+    call disk_load
 
-    mov dx, 0x73a6
+    mov dx, [0x9000]
+    call print_hex
+
+    mov dx, [0x9000 + 512]
     call print_hex
 
     jmp $
 
-    titre:
-        db 'Bienvenu sur ExpensiveOS', 0
-    title:
-        db 'Welcome to ExpensiveOS', 0
-
     ; Remplie 510 octets 0, depuis l'adresse du début du programme($$) jusqu'à l'adresse actuel du programme($)
     times 510-($-$$) db 0
     dw 0xaa55
+
+    times 256 dw 0xdada
+    times 256 dw 0xface
