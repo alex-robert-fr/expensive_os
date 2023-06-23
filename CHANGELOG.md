e3e380664dce756f08c12e4073265df011788b5d
0x7x00 <contact.alexrobert04@gmail.com>
2023-06-23 14:45:54 +0200

    Affiche 'Hello World' grace aux interruption BIOS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
+## [0.0.2a] - (23.06.2023)
+### Modification
+- Fichier `boot.s`
+    - Affiche 'Hello World' grace aux interruption BIOS
 ## [0.0.1a] - (23.06.2023)
 ### Ajouts
 - Fichier `Makefile`
--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,5 +1,27 @@
-start:
-	jmp start
+org 0x7c00
 
+xor ax, ax
+mov ds, ax
+cld				; CLear Direction flag, met le flag DF à 0. Ainsi, l'index de la chaîne sera incrémenté (non indécrementé.
+
+mov si, msg
+call bios_print
+
+_start:
+	jmp _start
+
+msg	db 'Hello World', 13, 10, 0
+
+bios_print:
+	lodsb		; Charge l'élément pointé par DS dans AX.
+	or al, al	; Verifie la fin de la chaine
+	jz done
+	mov ah, 0x0e
+	mov bh, 0
+	int 0x10
+	jmp bios_print
+done:
+
+	ret
 	times 510-($-$$) db 0
 	dw 0xaa55
